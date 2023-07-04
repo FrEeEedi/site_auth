74568377c7a444aa6587f3e3d7d1b42642879639
Sergej <s.podbegalin@choose.digital>
2023-07-04 19:43:50 +0200

    Методы получения юзера по логину, получение юзера по id, обновление профиля юзера

--- Classes/UserStore.php
+++ Classes/UserStore.php
@@ -25,12 +25,54 @@ class UserStore
 
         if ($login_check_query->num_rows) {
 
-            $result = $this->getUserID($login);
+            $result = $this->getUser($login);
 
             return $result;
         }
 
-        return 'Неправильный логи или пароль';
+        return 'Неправильный логин или пароль';
+    }
+
+    public function getUser($login)
+    {
+        $query = "SELECT * FROM users WHERE login = '$login'";
+        $user_result = $this->db_connection->query($query);
+
+        if ($user_result->num_rows) {
+
+            $user_array = $this->resultToArray($user_result);
+
+            return [
+                'result' => true,
+                'user' => $user_array
+            ];
+        }
+
+        return [
+            'result' => false,
+            'message' => 'Неправильный логин'
+        ];
+    }
+
+    public function getUserByID($id)
+    {
+        $query = "SELECT * FROM users WHERE id = '$id'";
+        $user_result = $this->db_connection->query($query);
+
+        if ($user_result->num_rows) {
+
+            $user_array = $this->resultToArray($user_result);
+
+            return [
+                'result' => true,
+                'user' => $user_array
+            ];
+        }
+
+        return [
+            'result' => false,
+            'message' => 'Пользователя с таким id нет'
+        ];
     }
 
     public function getUserID($login)
@@ -42,10 +84,24 @@ class UserStore
 
             $user_array = $this->resultToArray($user_result);
 
-            return $user_array['id'];
+            return [
+                'result' => true,
+                'id' => $user_array['id']
+            ];
         }
 
-        return 'Неправильный логин';
+        return [
+            'result' => false,
+            'message' => 'Неправильный логин'
+        ];
+    }
+
+    public function updateUserInfo($column, $value, $user_id)
+    {
+        $query = "UPDATE users SET $column = '$value' WHERE id = $user_id";
+        $result = $this->db_connection->query($query);
+
+        return $result;
     }
 
     public function resultToArray($result)
