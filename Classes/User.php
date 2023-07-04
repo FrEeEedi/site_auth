c6c947287cfa78fcc447db0079fa33267b73e038
Sergej <s.podbegalin@choose.digital>
2023-07-04 19:40:19 +0200

    Метод получения пользователя по id в классе User

--- Classes/User.php
+++ Classes/User.php
@@ -6,12 +6,22 @@ class User
 {
     private $user_store;
 
-    private function __construct(DataBase $db)
+    public function __construct(DataBase $db)
     {
         $this->user_store = new UserStore($db);
     }
 
     public function getUserByID($id)
     {
+        $result = $this->user_store->getUserByID($id);
+
+        return $result;
+    }
+
+    public function updateUserInfo($column, $value, $user_id)
+    {
+        $result = $this->user_store->updateUserInfo($column, $value, $user_id);
+
+        return $result;
     }
 }
